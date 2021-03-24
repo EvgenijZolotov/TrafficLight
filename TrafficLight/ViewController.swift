e0abdfd033d141e3e91ece7b99d890811c7277bd
EvgenijZolotov <evgenij.zolotov@gmail.com>
2021-03-24 17:37:05 +0300

    The graphic part is complete

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        toggleButton.layer.cornerRadius = 15
+        redView.layer.cornerRadius = 70
+        redView.alpha = 0.3
+        
+        
+        yellowView.layer.cornerRadius = 70
+        yellowView.alpha = 0.3
+        
+        greenView.layer.cornerRadius = 70
+        greenView.alpha = 0.3
     }
 
-
+    @IBAction func showButtonPressed(_ sender: Any) {
+        
+        if redView.alpha == 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else if greenView.alpha == 1 {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        } else {
+            redView.alpha = 1
+            toggleButton.setTitle("Нажми меня", for: .normal)
+        }
+    }
+    
 }
 
