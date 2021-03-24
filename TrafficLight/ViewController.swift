be4e9a0d621e7553af688e57e08b5869c22524c4
EvgenijZolotov <evgenij.zolotov@gmail.com>
2021-03-24 15:41:18 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Evgenij Zolotov on 3/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
